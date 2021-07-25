cf353c08d2b83799bf61f12989cfa2a917a5ffe3
eikur <jorge.soriano.pinedo@gmail.com>
2021-07-25 21:49:35 +0200

    Add Localization to Utils

---
+++ Runtime/LocalizationService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using BayatGames.Serialization.Formatters.Json;
+using System;
+
+[CreateAssetMenu(fileName = "LocalizationService", menuName = "Services/LocalizationService")]
+public class LocalizationService : Service
+{
+    [SerializeField] private SystemLanguage _currentLanguage;
+
+    private Dictionary<SystemLanguage, string> _localizationFiles = new Dictionary<SystemLanguage, string>
+    {
+        {SystemLanguage.English, "en" },
+        {SystemLanguage.Spanish, "es" }
+    };
+
+    Dictionary<string, string> _dictionary = new Dictionary<string, string>();
+
+    SystemLanguage _defaultLanguage = SystemLanguage.Spanish;
+
+    public override void AddToContainer(IServiceContainer container)
+    {
+        var localizationService = ScriptableObject.CreateInstance<LocalizationService>();
+        localizationService._currentLanguage = _currentLanguage;
+        container.Register<LocalizationService>(localizationService);
+        container.Register<Service>(localizationService);
+        container.Register<IDisposable>(localizationService);
+    }
+
+    public override void Initialize()
+    {
+        if (!_localizationFiles.ContainsKey(_currentLanguage))
+        {
+            _currentLanguage = _defaultLanguage;
+        }
+
+        TextAsset languageLoaded = Resources.Load<TextAsset>(GetLanguageFileName(_currentLanguage));
+        if (languageLoaded == null)
+        {
+            languageLoaded = Resources.Load<TextAsset>(GetLanguageFileName(_defaultLanguage));
+        }
+        _dictionary = JsonFormatter.DeserializeObject(languageLoaded.ToString(), typeof(Dictionary<string, string>)) as Dictionary<string, string>;
+
+        string GetLanguageFileName(SystemLanguage language)
+        {
+            return "language-" + _localizationFiles[language];
+        }
+    }
+
+    public string Localize(string key)
+    {
+        if (_dictionary.ContainsKey(key))
+        {
+            return _dictionary[key];
+        }
+
+        return "#"+ key;
+    }
+}
+
---
+++ Runtime/LocalizedText.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+
+public class LocalizedText : MonoBehaviour
+{
+    [SerializeField] private string _key;
+    TMP_Text _textToLocalize;
+
+    private void Awake()
+    {
+        if (string.IsNullOrEmpty(_key))
+        {
+            return;
+        }
+
+        _textToLocalize = GetComponent<TextMeshProUGUI>();
+        if (_textToLocalize == null)
+        {
+            _textToLocalize = GetComponent<TextMeshPro>();
+        }
+
+        if (_textToLocalize != null)
+        {
+            _textToLocalize.text = Services.Instance.Resolve<LocalizationService>().Localize(_key);
+        }
+    }
+}
